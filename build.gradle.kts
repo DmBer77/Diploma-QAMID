c38f7e906a87360ad2e3340b7cacee5aef5d2939
Dmitry Berezovsky <dm.berezovsky@gmail.com>
2023-05-26 16:46:15 +0300

    added allure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("io.qameta.allure") version "2.11.2"
+
 }
 
 group = "ru.netology"
@@ -10,9 +12,17 @@ repositories {
 }
 
 dependencies {
-    implementation("io.appium:java-client:7.6.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    implementation ("io.appium:java-client:7.6.0")
+
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+
+    testImplementation("org.aspectj:aspectjweaver:1.9.7")
+    testImplementation ("io.qameta.allure:allure-junit5:2.20.1")
+    testImplementation ("io.qameta.allure:allure-commandline:2.20.1")
+    testImplementation ("io.qameta.allure:allure-assertj:2.20.1")
+    testImplementation ("io.qameta.allure:allure-rest-assured:2.20.1")
+    testImplementation ("io.qameta.allure:allure-java-commons:2.20.1")
 }
 
 tasks.test {
